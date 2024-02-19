8ff4f47910034eb8211c10f9a90c9f62c3ea5c4a
YaelDonat <donatyael@gmail.com>
2024-02-19 11:07:56 +0100

    add pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,47 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule } from '@nestjs/config';
+import pino from 'pino';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: process.env.NODE_ENV !== 'production' ? 'debug' : 'info',
+        formatters: {
+          level: (label, number) => {
+            return { level: label };
+          },
+        },
+
+        customReceivedMessage: function (req, res) {
+          return `request ${req.id} received: ${req.method} ${req.url}`;
+        },
+
+        customSuccessMessage: function (req, res) {
+          return `${req.method} ${req.url} completed`;
+        },
+
+        customErrorMessage: function (req, res, err) {
+          return `request ${req.id} to ${req.method} ${req.url} errored with status code: ${res.statusCode} message: ${err.message}`;
+        },
+
+        stream: pino.destination({
+          dest: `./logs/${new Date().toLocaleDateString('en-US').replace(/\//g, '-')}.log`,
+          minLength: 4096,
+          sync: false,
+        }),
+
+        transport:
+          process.env.NODE_ENV !== 'production'
+            ? { target: 'pino-pretty' }
+            : undefined,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+  await app.listen(process.env.NESTJS_PORT);
+
+  console.log('Server listening on port : ' + process.env.NESTJS_PORT);
 }
 bootstrap();
