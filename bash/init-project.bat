748452fa4affc98ff18ae19a59e4bed3fbc33956
Anojan Yogeswaran <ayogeswaran@netwo.com>
2024-02-23 16:24:07 +0100

    fix bash

--- bash/init-project.bat
+++ bash/init-project.bat
@@ -1,18 +1,36 @@
 @echo off
 
 REM Demander le port NestJS
-set /p nestjs_port="Entrez le port pour NestJS (par exemple, 3000): "
+set /p nestjs_port="Entrez le port pour NestJS (exemple, 3000): "
 echo NESTJS_PORT=%nestjs_port% > .env
 
 REM Demander le DATABASE_URL
-set /p db_url="Entrez votre DATABASE_URL: "
+set /p db_url="Entrez votre DATABASE_URL (exemple: postgresql://user:password@localhost:5432/dbname?schema=public): "
 echo DATABASE_URL="%db_url%" > database\prisma\.env
 
 echo Fichiers .env créés avec succès.
 
+REM Vérifier Docker et Docker Compose
+docker -v > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Docker n'est pas installé. Veuillez installer Docker avant de continuer.
+    exit /b 1
+)
+
+docker-compose -v > nul 2>&1
+if %errorlevel% neq 0 (
+    echo docker-compose n'est pas installé. Veuillez installer docker-compose avant de continuer.
+    exit /b 1
+)
+
+REM Démarrer les services Docker
+echo Lancement des services Docker...
+cd database
+docker-compose up -d
+
 REM Exécuter les commandes Prisma
 echo Exécution de Prisma Generate et Migrate...
-cd database\prisma
+cd prisma
 call npx prisma generate
 call npx prisma migrate dev
 cd ..\..
@@ -22,4 +40,3 @@ echo Lancement du script de remplissage de la base de données...
 call node scrapers\script.js
 
 echo Initialisation terminée.
-
--- bash/init-project.sh
+++ bash/init-project.sh
@@ -5,20 +5,38 @@ read -p "Entrez le port pour NestJS (par exemple, 3000): " nestjs_port
 echo "NESTJS_PORT=$nestjs_port" > .env
 
 # Demander le DATABASE_URL
-read -p "Entrez votre DATABASE_URL: " db_url
+read -p "Entrez votre DATABASE_URL (exemple: postgresql://user:password@localhost:5432/dbname?schema=public): " db_url
 echo "DATABASE_URL=\"$db_url\"" > database/prisma/.env
 
 echo "Fichiers .env créés avec succès."
 
+# Vérifier Docker et Docker Compose
+if ! command -v docker &> /dev/null
+then
+    echo "Docker n'est pas installé. Veuillez installer Docker avant de continuer."
+    exit 1
+fi
+
+if ! command -v docker-compose &> /dev/null
+then
+    echo "docker-compose n'est pas installé. Veuillez installer docker-compose avant de continuer."
+    exit 1
+fi
+
+# Démarrer les services Docker
+echo "Lancement des services Docker..."
+cd database
+docker-compose up -d
+
 # Exécuter les commandes Prisma
 echo "Exécution de Prisma Generate et Migrate..."
-cd database/prisma
+cd prisma
 npx prisma generate
 npx prisma migrate dev
 cd ../..
 
 # Remplir la base de données
 echo "Lancement du script de remplissage de la base de données..."
-node scraper/script.js
+node scrapers/script.js
 
 echo "Initialisation terminée."
